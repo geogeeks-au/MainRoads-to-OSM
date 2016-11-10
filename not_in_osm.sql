ccbe471824cd7e04e09b5888d99dcb6e074a5640
Sam Wilson <sam@samwilson.id.au>
2016-11-10 07:56:38 -0500

    Update

--- not_in_osm.sql
+++ not_in_osm.sql
@@ -3,11 +3,11 @@ DROP MATERIALIZED VIEW IF EXISTS not_in_osm;
 CREATE MATERIALIZED VIEW not_in_osm AS
 WITH buffered_osm AS (
 	SELECT 
-		ST_Transform( ST_Union(ST_Buffer(way, 10.0)), 4283) AS ways
-	FROM planet_osm_roads
-	WHERE ST_Transform(ST_MakeEnvelope(
-                117.074, -33.4207, 117.1745, -33.3163, 4283
-	), 900913) && way
+		ST_Transform( ST_Union(ST_Buffer(way, 20.0)), 4283) AS ways
+	FROM planet_osm_line
+	WHERE 
+		highway IS NOT NULL 
+		-- AND ST_Transform(ST_MakeEnvelope( 117.074, -33.4207, 117.1745, -33.3163, 4283), 900913) && way
 )
 SELECT 
 	row_number() over() as id,
@@ -17,8 +17,6 @@ SELECT
 	mrwa.road_name AS mrwa_name,
 	ST_Difference(ST_SetSRID(mrwa.geom, 4283), (select ways from buffered_osm)) AS geom
 FROM mrwa
-WHERE ST_MakeEnvelope(
-                117.074, -33.4207, 117.1745, -33.3163, 4283
-) && mrwa.geom
+-- WHERE ST_MakeEnvelope( 117.074, -33.4207, 117.1745, -33.3163, 4283 ) && mrwa.geom
 ;
 
